c9c6aaa4604651aa091fb7190257335d06c90d14
Deepak Sharma <46669654+deepakprogrammer@users.noreply.github.com>
2024-05-31 04:22:12 +0530

    just workspace setup

---
+++ src/test/java/org/acme/stayease/StayEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.acme.stayease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
